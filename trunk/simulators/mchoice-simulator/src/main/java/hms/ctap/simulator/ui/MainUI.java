99f5e62310bbf68439467c221251b4f9dedd52d9
manoramahp@gmail.com <manoramahp@gmail.com@530361ac-b48f-00dc-8818-b0b50dbf66b0>
2011-04-22 08:12:52 +0000

    adding header - logo and name

--- trunk/simulators/mchoice-simulator/src/main/java/hms/ctap/simulator/ui/MainUI.java
+++ trunk/simulators/mchoice-simulator/src/main/java/hms/ctap/simulator/ui/MainUI.java
@@ -5,8 +5,14 @@
 
 package hms.ctap.simulator.ui;
 
+
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import hms.ctap.simulator.ui.sms.SmsNcsUIService;
 import hms.ctap.simulator.ui.tab.impl.TabViewImpl;
@@ -23,10 +29,12 @@ public class MainUI {
     public Component getRootLayout() {
 
         VerticalLayout rootLayout = new VerticalLayout();
-        rootLayout.addComponent(createHeader());
-
-        Component tabPanel = createMainUI().createTabSheetPanel();
-        rootLayout.addComponent(tabPanel);
+        rootLayout.setStyleName("root-layout");
+        Panel mainPanel = new Panel();
+        mainPanel.addComponent(createHeader());
+        Component tabSheet = createMainUI().createTabSheetPanel();
+        mainPanel.addComponent(tabSheet);
+        rootLayout.addComponent(mainPanel);
         return rootLayout;
     }
 
@@ -42,21 +50,20 @@ public class MainUI {
         smsTabView.init();
         ussdTabView.init();
 
-
         return new TabSheetPanel(smsTabView, ussdTabView);
     }
 
     private Component createHeader() {
 
         HorizontalLayout header = new HorizontalLayout();
-//        header.setWidth("100%");
-//        Embedded logo = new Embedded("", new ThemeResource("hms_logo.jpg"));
-//        header.addComponent(logo);
-//        header.setComponentAlignment(logo, Alignment.TOP_LEFT);
-//        Label headerLabel = new Label("Simulator");
-//        headerLabel.setStyleName("simulator-header");
-//        header.addComponent(headerLabel);
-//        header.setComponentAlignment(headerLabel, Alignment.MIDDLE_RIGHT);
+        header.setWidth("100%");
+        Embedded logo = new Embedded("", new ThemeResource("images/hms_logo.jpg"));
+        header.addComponent(logo);
+        header.setComponentAlignment(logo, Alignment.TOP_LEFT);
+        Label headerLabel = new Label("mChoice Simulator");
+        headerLabel.setStyleName("simulator-header");
+        header.addComponent(headerLabel);
+        header.setComponentAlignment(headerLabel, Alignment.MIDDLE_RIGHT);
         return header;
     }
 
