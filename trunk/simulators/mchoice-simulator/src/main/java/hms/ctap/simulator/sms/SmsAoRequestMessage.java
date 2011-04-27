c4f73a6f9914c48d8d9c01c737a2c5354b340f18
manoramahp@gmail.com <manoramahp@gmail.com@530361ac-b48f-00dc-8818-b0b50dbf66b0>
2011-04-27 12:23:56 +0000

    adding SmsAoRequestMessage class

---
+++ trunk/simulators/mchoice-simulator/src/main/java/hms/ctap/simulator/sms/SmsAoRequestMessage.java
@@ -0,0 +1,66 @@
+/*
+ *   (C) Copyright 2010-2011 hSenid Software International (Pvt) Limited.
+ *   All Rights Reserved.
+ *
+ *   These materials are unpublished, proprietary, confidential source code of
+ *   hSenid Software International (Pvt) Limited and constitute a TRADE SECRET
+ *   of hSenid Software International (Pvt) Limited.
+ *
+ *   hSenid Software International (Pvt) Limited retains all title to and intellectual
+ *   property rights in these materials.
+ *
+ */
+package hms.ctap.simulator.sms;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hms
+ * Date: 4/27/11
+ * Time: 12:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmsAoRequestMessage {
+
+    private String address;
+    private String message;
+    private String appId;
+    private String password;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+         return ("{address: " + address + " ,message: " + message + " ,appId: " + appId + " ,password: " + password + " }");
+    }
+}
