12f0260d80a40d79c8ccf48aae2580da9cdee52f
manoramahp@gmail.com <manoramahp@gmail.com@530361ac-b48f-00dc-8818-b0b50dbf66b0>
2011-04-27 12:25:45 +0000

    implementing send message method

--- trunk/simulators/mchoice-simulator/src/main/java/hms/ctap/simulator/sms/SmsMessageSender.java
+++ trunk/simulators/mchoice-simulator/src/main/java/hms/ctap/simulator/sms/SmsMessageSender.java
@@ -12,10 +12,45 @@
  */
 package hms.ctap.simulator.sms;
 
+
+import java.io.IOException;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+
 /**
  * $LastChangedDate$
  * $LastChangedBy$
  * $LastChangedRevision$
  */
 public class SmsMessageSender {
+    private static SmsMessageSender smsMessageSender = new SmsMessageSender();
+    private HttpClient httpClient;
+
+    private SmsMessageSender() {
+        httpClient = new HttpClient();
+    }
+
+    public void sendMessage (String url, String address, String message) throws IOException {
+
+        final PostMethod postMethod = new PostMethod(url);
+        postMethod.addRequestHeader("CONTENT_TYPE", "application/x-www-form-urlencoded");
+        postMethod.addRequestHeader("ACCEPT", "text/xml");
+        postMethod.addParameter("version", "1.0");
+        postMethod.addParameter("address",address);
+        postMethod.addParameter("message", message);
+        String correlator = String.valueOf(Math.random());
+        postMethod.addParameter("correlator", correlator);
+        System.out.println("Sending SMS to application [ address: "+ address + " ,message: "+message + " ,correlator: "+ correlator + " ]");
+        httpClient.executeMethod(postMethod);
+        System.out.println(postMethod.getResponseBodyAsString());
+
+    }
+
+    public static SmsMessageSender getInstance() {
+
+        if (smsMessageSender == null) {
+            smsMessageSender = new SmsMessageSender();
+        }
+        return smsMessageSender;
+    }
 }
