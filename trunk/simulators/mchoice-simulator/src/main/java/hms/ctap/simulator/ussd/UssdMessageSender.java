65f7e69372a19f4dee6e29ebc52df91ce5517504
jasondevj@gmail.com <jasondevj@gmail.com@530361ac-b48f-00dc-8818-b0b50dbf66b0>
2011-04-29 11:19:30 +0000

    committing after adding terminate message schedule

--- trunk/simulators/mchoice-simulator/src/main/java/hms/ctap/simulator/ussd/UssdMessageSender.java
+++ trunk/simulators/mchoice-simulator/src/main/java/hms/ctap/simulator/ussd/UssdMessageSender.java
@@ -15,6 +15,7 @@ package hms.ctap.simulator.ussd;
 import com.google.gson.Gson;
 import hms.sdp.ussd.MchoiceUssdMessage;
 import hms.sdp.ussd.impl.UssdAtRequestMessage;
+import hms.sdp.ussd.impl.UssdTerminateMessage;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.eclipse.jetty.http.HttpHeaders;
@@ -22,6 +23,9 @@ import org.eclipse.jetty.http.HttpHeaders;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * $LastChangedDate$
@@ -32,16 +36,17 @@ public class UssdMessageSender {
 
     private static UssdMessageSender ussdMessageSender;
 
-    private Map<String, String> conversationMap;
+    private Map<String, Conversation> conversationMap;
     private HttpClient httpClient;
+    private ScheduledExecutorService executorService;
 
-    private UssdMessageSender () {
+    private UssdMessageSender() {
         httpClient = new HttpClient();
     }
 
-    public void sendMessage (String url, String address, String message) throws IOException {
+    public void sendMessage(String url, String address, String message) throws IOException {
 
-        final PostMethod postMethod = createMethod(url, address);
+        final PostMethod postMethod = createPostClient(url, address, MchoiceUssdMessage.USSD_MESSAGE);
         final UssdAtRequestMessage ussdAtRequestMessage = new UssdAtRequestMessage();
         ussdAtRequestMessage.setMessage(message);
         ussdAtRequestMessage.setAddress(address);
@@ -55,23 +60,23 @@ public class UssdMessageSender {
         System.out.println(postMethod.getResponseBodyAsString());
     }
 
-    private PostMethod createMethod(String url, String address) {
+    private PostMethod createPostClient(String url, String address, String messageType) {
         final PostMethod postMethod = new PostMethod(url);
         postMethod.addRequestHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        postMethod.addRequestHeader(MchoiceUssdMessage.USSD_MESSAGE_TYPE, MchoiceUssdMessage.USSD_MESSAGE);
-        postMethod.addRequestHeader(MchoiceUssdMessage.CONVERSATION, checkAddNewConversation(address));
+        postMethod.addRequestHeader(MchoiceUssdMessage.USSD_MESSAGE_TYPE, messageType);
+        postMethod.addRequestHeader(MchoiceUssdMessage.CONVERSATION, checkAddNewConversation(address, url));
         postMethod.addRequestHeader(MchoiceUssdMessage.REQUEST_VERSION, "1.0");
         postMethod.addRequestHeader("X-Requested-Shortcode", "#141#1000");
         return postMethod;
     }
 
-    private String checkAddNewConversation(String address) {
-        String conversationId = conversationMap.get(address);
+    private String checkAddNewConversation(String address, String url) {
+        Conversation conversationId = conversationMap.get(address);
         if (conversationId == null) {
-            conversationId = String.valueOf(Math.random() * 10000000000l);
+            conversationId = new Conversation(String.valueOf(Math.random() * 10000000000l), url);
             conversationMap.put(address, conversationId);
         }
-        return conversationId;
+        return conversationId.getConversationId();
     }
 
     public boolean clearConversation(String address) {
@@ -85,8 +90,63 @@ public class UssdMessageSender {
     public static UssdMessageSender getInstance() {
         if (ussdMessageSender == null) {
             ussdMessageSender = new UssdMessageSender();
-            ussdMessageSender.conversationMap = new HashMap<String, String>();
+            final HashMap<String, Conversation> hashMap = new HashMap<String, Conversation>();
+            ussdMessageSender.conversationMap = hashMap;
+            ussdMessageSender.executorService = Executors.newSingleThreadScheduledExecutor();
+            ussdMessageSender.executorService.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        execute(ussdMessageSender, hashMap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 60, 60, TimeUnit.SECONDS);
         }
         return ussdMessageSender;
+    }
+
+    private static void execute(UssdMessageSender ussdMessageSender, HashMap<String, Conversation> hashMap) throws IOException {
+        if (!hashMap.isEmpty()) {
+            for (Map.Entry<String, Conversation> entry : hashMap.entrySet()) {
+                final PostMethod postMethod =
+                        ussdMessageSender.createPostClient(entry.getValue().getUrl(), entry.getKey(),
+                        MchoiceUssdMessage.USSD_TERMINATE_MESSAGE);
+                final UssdTerminateMessage ussdMessage = new UssdTerminateMessage();
+                ussdMessage.setCorrelationId("89898989898");
+                ussdMessage.setAddress(entry.getKey());
+                postMethod.setRequestBody(new Gson().toJson(ussdMessage));
+                hashMap.remove(entry.getKey());
+                ussdMessageSender.httpClient.executeMethod(postMethod);
+            }
+        }
+    }
+
+    private class Conversation {
+
+        private String conversationId;
+        private String url;
+
+        private Conversation(String conversationId, String url) {
+            this.conversationId = conversationId;
+            this.url = url;
+        }
+
+        public String getConversationId() {
+            return conversationId;
+        }
+
+        public void setConversationId(String conversationId) {
+            this.conversationId = conversationId;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
     }
 }
