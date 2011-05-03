6828e15563079916c8bfdb336d26b11e9f01815f
manoramahp <manoramahp@gmail.com>
2011-05-03 06:12:00 +0000

    adding sample ussd application

---
+++ sample-applications/ussd/java/sample/src/test/java/hms/sdp/ussd/sample/client/MessageSendTest.java
@@ -0,0 +1,33 @@
+/*
+ *   (C) Copyright 2010-2011 hSenid Software International (Pvt) Limited.
+ *   All Rights Reserved.
+ *
+ *   These materials are unpublished, proprietary, confidential source code of
+ *   hSenid Software International (Pvt) Limited and constitute a TRADE SECRET
+ *   of hSenid Software International (Pvt) Limited.
+ *
+ *   hSenid Software International (Pvt) Limited retains all title to and intellectual
+ *   property rights in these materials.
+ *
+ */
+package hms.sdp.ussd.sample.client;
+
+import hms.sdp.ussd.MchoiceUssdException;
+import hms.sdp.ussd.MchoiceUssdResponse;
+import hms.sdp.ussd.client.MchoiceUssdSender;
+import org.junit.Test;
+
+/**
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ */
+public class MessageSendTest {
+
+    @Test
+    public void testSendMessage() throws MchoiceUssdException {
+//        MchoiceUssdSender ussdSender = new MchoiceUssdSender("http://127.0.0.1:2000/ussd/receive", "appid", "pass");
+//        final MchoiceUssdResponse mchoiceUssdResponse = ussdSender.sendMessage("Test Message", "1234", "00001", false);
+//        System.out.println(mchoiceUssdResponse);
+    }
+}
